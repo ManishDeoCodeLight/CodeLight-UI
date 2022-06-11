3bda40e926f41707789619c14e6ec47ea3b08f85
ManishDeoCodeLight <107293868+ManishDeoCodeLight@users.noreply.github.com>
2022-06-11 16:21:35 +0530

    config

--- src/app/external-config-dashboard/config-dashboard.service.ts
+++ src/app/external-config-dashboard/config-dashboard.service.ts
@@ -11,6 +11,8 @@ export class ConfigDashboardService {
 
 
   private readonly  configUrl = 'assets/region.json';
+  
+  
 
   getConfig() {
     return this.http.get<region[]>(this.configUrl);
@@ -18,5 +20,17 @@ export class ConfigDashboardService {
 
   getLogInfo() {
     return this.http.get<any[]>("assets/logInfo.json");
+  }
+  
+  getRegionWiseConfig(region: string) {
+    let configFetchURL;
+    if (region === "india") {
+      configFetchURL= 'assets/india.json';
+    } else if (region === "usa") {
+      configFetchURL= 'assets/usa.json';
+    } else{
+      configFetchURL= 'assets/uk.json'
+    }
+    return this.http.get<any>(configFetchURL);
   }
 }
