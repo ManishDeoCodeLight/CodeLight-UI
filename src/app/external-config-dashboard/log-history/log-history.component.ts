f6cee124aa3bd95e2b15ebc4fe7559c3d6e834e0
ManishDeoCodeLight <107293868+ManishDeoCodeLight@users.noreply.github.com>
2022-06-11 22:44:30 +0530

    commit

--- src/app/external-config-dashboard/log-history/log-history.component.ts
+++ src/app/external-config-dashboard/log-history/log-history.component.ts
@@ -1,22 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { ConfigDashboardService } from './../config-dashboard.service'
-import { columnInfo } from  './../column-info.component';
-import { Subscription } from 'rxjs';
-
-export interface ColumnInfo {
-  continent: string;
-  country: number;
-  president: number;
-  primeMinister: string;
-  largestCity: string;
-  literacyRate: number;
-  population: number;
-  festival: string;
-}
-
-const ELEMENT_DATA: ColumnInfo[] = [
-  
-];
+import { MatSort } from '@angular/material/sort';
+import { MatTableDataSource } from '@angular/material/table';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatPaginator } from '@angular/material/paginator';
 
 @Component({
   selector: 'log-history',
@@ -24,17 +12,31 @@ const ELEMENT_DATA: ColumnInfo[] = [
   styleUrls: ['./log-history.component.scss']
 })
 export class LogHistoryComponent implements OnInit {
+
+  @ViewChild(MatSort) sort: MatSort;
+  @ViewChild(MatPaginator) paginator: MatPaginator;
+  dataSource = new MatTableDataSource<any>();
   constructor(private configDashboardService: ConfigDashboardService ) { }
 
   ngOnInit(): void {
     this.getLogHistory();
   }
 
-  displayedColumns: string[] = ['continent', 'country', 'president', 'primeMinister', 'largestCity', 'literacyRate', 'population', 'festival'];
-  dataSource = ELEMENT_DATA;
+  ngAfterViewInit(): void {
+    this.dataSource.sort = this.sort;
+    this.dataSource.paginator = this.paginator;
+  }
+
+  displayedColumns: string[] = ["propertyLevel", "label", "propName", "oldValue", "newValue", "updatedBy", "updatedOn"];
+  
+
+  public doFilter = (event: Event) => {
+    const filterValue = (event.target as HTMLInputElement).value;
+    this.dataSource.filter = filterValue.trim().toLocaleLowerCase();
+  }
 
   private getLogHistory() {
-    this.configDashboardService.getLogInfo().subscribe(data => {this.dataSource = data;});
+    this.configDashboardService.getLogInfo().subscribe(data => {this.dataSource.data = data;});
   }
 
 }
